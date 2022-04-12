0389f52d6758aadf44b8059567fd33713c463c43
vijay451 <vijayb@grhombus.com>
2022-04-12 10:30:27 +0530

    UI implemented

---
+++ app/components/add-task/add.js
@@ -0,0 +1,14 @@
+import Component from '@ember/component';
+import { inject as service } from '@ember/service';
+
+export default Component.extend({
+  init() {
+    this._super(...arguments);
+  },
+
+  actions: {
+    addbtn() {
+      
+    },
+  },
+});
